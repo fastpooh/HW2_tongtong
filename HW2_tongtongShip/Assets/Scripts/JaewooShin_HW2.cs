b89b64736e2f4a1e4b9cba1e978bc6474d080f33
jaewoo356 <jaewoo356@gmail.com>
2023-07-22 22:19:58 +0900

    homework

---
+++ HW2_tongtongShip/Assets/Scripts/JaewooShin_HW2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JaewooShin_HW2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("JaewooShin HW2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
