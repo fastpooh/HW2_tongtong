baedb8430a066cfce6b5c34fea90751498770c55
fastpooh <cwchris@naver.com>
2023-07-30 13:49:42 +0900

    homework finish

--- HW2_tongtongShip/Assets/Scripts/Jaegyu_HW2.cs
+++ HW2_tongtongShip/Assets/Scripts/Jaegyu_HW2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Euijun_HW2 : MonoBehaviour
+public class Jaegyu_HW2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- HW2_tongtongShip/Assets/Scripts/Suhin_HW2.cs
+++ HW2_tongtongShip/Assets/Scripts/Suhin_HW2.cs
@@ -7,7 +7,7 @@ public class Suhin_HW2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.log("Suhin_HW2");
+        Debug.Log("Suhin_HW2");
     }
 
     // Update is called once per frame
